93843b6589d2b8318d78a31eb4d63c35a38701e0
mc_gee02 <gerhard@poppgerhard.at>
2024-02-23 16:41:46 +0100

    armenia

--- resources/views/livewire/icon-info.blade.php
+++ resources/views/livewire/icon-info.blade.php
@@ -3,7 +3,12 @@
      x-on:iconinfo.window="loading = true"
      x-on:iconinfoloaded.window="loading = false"
      class="relative">
-    <div x-cloak x-show="loading" class="absolute inset-0 bg-white/50"></div>
+    <div x-cloak x-show="loading" class="absolute inset-0 bg-white/50 flex items-center justify-center">
+        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-orange-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+    </div>
     <h2 @class([
         "text-2xl font-bold",
         "text-orange-800/25" => empty($country),
