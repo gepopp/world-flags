fa2a4f174e3072366fde8612d59131af27a6add9
mc_gee02 <gerhard@poppgerhard.at>
2024-02-24 09:14:38 +0100

    article extend

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -12,24 +12,26 @@
             </div>
             <h1 class="text-orange-800 font-bold text-5xl">{{ $article->title }}</h1>
 
-
-            <div x-data="{ icons : {{ $icons }} }"
-                 x-init="$dispatch('iconinfo', { country: '{{ $article->country }}', dir: 'base' });"
-                class="my-10 col-span-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 2xl:grid-cols-6 gap-4 border border-slate-300">
-                <template x-for="icon in icons" :key="icon.url">
-                    <div x-data="copyCode"
-                         x-on:click="
+            <div class="my-10">
+                <h5 class="text-orange-800 text-lg font-semibold">Available Icons</h5>
+                <div x-data="{ icons : {{ $icons }} }"
+                     x-init="$dispatch('iconinfo', { country: '{{ $article->country }}', dir: 'base' });"
+                     class="col-span-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 2xl:grid-cols-6 gap-4 border border-slate-300">
+                    <template x-for="icon in icons" :key="icon.url">
+                        <div x-data="copyCode"
+                             x-on:click="
                                 copy();
                                 $dispatch('iconinfo', { country: icon.country, dir: icon.dir });
                                 $wire.set('selection', { country: icon.country, dir: icon.dir });
                                 "
-                         class="flex flex-col items-center aspect-square p-2 m-2 shadow-lg border border-slate-300 hover:border-orange-500 transition-all duration-300 rounded-lg cursor-pointer">
-                        <div class="w-full aspect-square flex items-center w-full rounded overflow-hidden">
-                            <img :src="icon.url" class="object-cover drop-shadow-xl w-full"/>
-                            <textarea x-ref="code" class="hidden" x-text="icon.code"></textarea>
+                             class="flex flex-col items-center aspect-square p-2 m-2 shadow-lg border border-slate-300 hover:border-orange-500 transition-all duration-300 rounded-lg cursor-pointer">
+                            <div class="w-full aspect-square flex items-center w-full rounded overflow-hidden">
+                                <img :src="icon.url" class="object-cover drop-shadow-xl w-full"/>
+                                <textarea x-ref="code" class="hidden" x-text="icon.code"></textarea>
+                            </div>
                         </div>
-                    </div>
-                </template>
+                    </template>
+                </div>
             </div>
 
 
